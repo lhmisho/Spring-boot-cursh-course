832af302be448f400037160c5b5c97d08159817b
lokman hossen <lokman@divine-it.net>
2020-04-03 19:26:22 +0600

    <application.properties> putting port number and some default value on properties

--- src/main/java/lokman/in/sampleDemoAPp/controller/RestEndPoints.java
+++ src/main/java/lokman/in/sampleDemoAPp/controller/RestEndPoints.java
@@ -1,9 +1,25 @@
 package lokman.in.sampleDemoAPp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestEndPoints {
+
+    @Value("${default.course.name}")
+    private String cName;
+
+    @Value("${default.course.chapterCount}")
+    private Integer cCount;
+
+    @RequestMapping(value = "/defaultValue")
+    public Course getDefaultValue(
+            @RequestParam(value="name", defaultValue="Spring boot", required = false) String name,
+            @RequestParam(value="chapterCount", defaultValue="2", required = false) Integer chapterCount
+    ){
+        return new Course(cName, cCount);
+    }
+
     @RequestMapping(value = "/course")
     public Course getEndpoints(
             @RequestParam(value="name", defaultValue="Spring boot", required = false) String name,
