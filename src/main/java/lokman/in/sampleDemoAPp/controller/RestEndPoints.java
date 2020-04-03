21f51af452c3aaf80f379d210a5337b25b9ad7cc
lokman hossen <lokman@divine-it.net>
2020-04-03 21:14:10 +0600

    <application-qa.properties> profile specific >> 1: create application-qa.properties 2: run >> ./mvnw clean compile 3: run mvn spring-boot:run -Dspring.profiles.active=qa

--- src/main/java/lokman/in/sampleDemoAPp/controller/RestEndPoints.java
+++ src/main/java/lokman/in/sampleDemoAPp/controller/RestEndPoints.java
@@ -1,10 +1,16 @@
 package lokman.in.sampleDemoAPp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+
 @RestController
 public class RestEndPoints {
+
+    @Autowired
+    CourseConfiguration courseConfiguration;
 
     @Value("${default.course.name}")
     private String cName;
@@ -18,6 +24,21 @@ public class RestEndPoints {
             @RequestParam(value="chapterCount", defaultValue="2", required = false) Integer chapterCount
     ){
         return new Course(cName, cCount);
+    }
+
+
+    @GetMapping("/getHirarchy")
+    public HashMap<String, Object> getConfigurationProperties(
+            @RequestParam(value="name", defaultValue="Spring boot", required = false) String name,
+            @RequestParam(value="chapterCount", defaultValue="2", required = false) Integer chapterCount
+    ){
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("name", courseConfiguration.getName());
+        map.put("courseCount", courseConfiguration.getChapterCount());
+        map.put("rating", courseConfiguration.getRating());
+        map.put("author", courseConfiguration.getAuther());
+
+        return map;
     }
 
     @RequestMapping(value = "/course")
